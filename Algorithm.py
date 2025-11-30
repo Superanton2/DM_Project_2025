b79f1b07f2c66d262a8fe7b1645c8f58e8de51a3
Shlyapkas <149323530+Shlyapkas@users.noreply.github.com>
2025-12-01 00:09:49 +0200

    Add files via upload

---
+++ Algorithm.py
@@ -0,0 +1,39 @@
+from graph_generator import graph_generator
+
+
+def algorithm(lst):
+    lst_of_graphs = graph_generator()
+
+    def get_vertex(lst: list):
+        vertexes = []
+        for a, b in lst:
+            vertexes.append(a)
+            vertexes.append(b)
+
+        vertexes_dict = {k: False for k in vertexes}
+        print(vertexes_dict)
+
+        return vertexes_dict
+
+
+    lst_sorted = []
+    def visit(dict1: dict, lst: list, vertex = None):
+        if vertex is None:
+            for v in dict1.keys():
+                if not dict1[v]:
+                    visit(dict1, lst, v)
+
+
+        dict1[vertex] = True
+
+        for a, b in lst:
+            if a == vertex and not dict1[b]:
+                visit(dict1, lst, b)
+        if vertex != None:
+            lst_sorted.insert(0, vertex)
+
+        return lst_sorted
+
+
+
+    return visit(get_vertex(lst_of_graphs), lst_of_graphs)
