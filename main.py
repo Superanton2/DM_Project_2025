da2b237dea38ef94740a16d00d7cf17f7a45a729
Anton <antontv2019@gmail.com>
2025-12-02 02:38:35 +0200

    rewrote the logic of the program  2. Added comments to generate graphs and moved the function for vertices to that file

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from Algorithm import visit, get_vertex
-
 from time import perf_counter
-from graph_generator import graph_generator
+
+from Algorithm import topological_sort
+from graph_generator import graph_generator, get_vertex
+
 
 
 def tester(vertex: int, density: float) -> float:
-    lst_of_graphs = graph_generator()
+    lst_of_graphs, adj_lst = graph_generator(vertex, density)
+
+
     vertex = get_vertex(lst_of_graphs)
+    # print(vertex)
 
 
     # Start the stopwatch / counter
     time_start = perf_counter()
 
     # функція
-    print(visit(vertex, lst_of_graphs))
+    topological_sort(lst_of_graphs, vertex)
 
     # Stop the stopwatch / counter
     time_stop = perf_counter()
 
     return time_stop - time_start
+
+
+# print(tester(20, 1))
+
 
 
 def main():
@@ -30,45 +38,14 @@ def main():
             density /= 100
             print(density)
 
+            test_time = tester(vertex, density)
 
 
-        # with open("test.csv", "a") as file:
-        #     pass
-        # pass
-        #
-# не менше п’яти різних значень щільності та не менше
-# десяти різних розмірів графу
+            with open("test.csv", "a") as file:
+                to_append = f"{vertex}, {density}, {test_time:10f}\n"
+                file.write(to_append)
+
 
 
 
 main()
-
-    # vertex = input("Enter number of vertexes in range 20 to 200: ")  # кількість вершин
-#     while True:
-#         while not vertex.isdigit():
-#             vertex = input("Enter digits only. Enter number of vertexes in range 20 to 200: ")
-#
-#         if 20 <= int(vertex) <= 200:
-#             vertex = int(vertex)
-#             break
-#         else:
-#             vertex = input("Enter digits in range 20 to 200 only. Enter number of vertexes in range 20 to 200: ")
-#
-#     density = input("Enter density in range 0 to 1: ") # очікувана щільність(відношення к-сті утворених графів до к-сті можливих)
-#     while True:
-#
-#         try:
-#             density = float(density)
-#         except ValueError:
-#             density = input("Enter numbers only. Enter density in range 0 to 1: ")
-#             continue
-#
-#         if 0 <= float(density) <= 1:
-#             density = float(density)
-#             break
-#         else:
-#             density = input("Enter density in range 0 to 1 only: ")
-
-
-# 0.0001031670
-# 0.00009045800000029303
