3a77afca2d9a6c190d35c4f55fd52850e5dfe0d6
Anton <antontv2019@gmail.com>
2025-12-08 13:57:46 +0200

    Refactor graph_generator and related functions

--- graph_generator.py
+++ graph_generator.py
@@ -1,23 +1,21 @@
 import random
 
 
-def graph_generator(vertex: int, density: float, test=None):
-
+def graph_generator(vertex: int, density: float):
     """
     генератор графів
     герерує список суміжностей та матрицю суміжності
-    при тестуванні (test == True) ми не виводимо інформацію про кількість графів задля візуальної чистоти терміналу
 
     :param vertex: кількість вершин. Від 20 до 200
     :param density: відсоток того як багато буде зʼєднано. Від 0 до 1
-    :return: список суміжності та матрицю суміжності
+    :return: список суміжностей та матрицю суміжності
     """
 
-    # створюємо словник усіх можливих пар, окрім петель (блок який перевіряє чи рівні числа) та циклів. Для кожної пари значення 0(нема графу) або 1(є граф)
+    # створюємо словник усіх можливих пар, окрім петель. Для кожної пари значення 0(нема графу) або 1(є граф)
 
     adjacency_matrix = {}
-    for num1 in range(1, vertex + 1):
-        for num2 in range(num1 + 1, vertex + 1):
+    for num1 in range(1, vertex+1):
+        for num2 in range(num1 + 1, vertex+1):
             if num1 == num2:
                 pass
             else:
@@ -26,8 +24,7 @@ def graph_generator(vertex: int, density: float, test=None):
 
     # пишемо алгоритм Ердеша-Реньї, який вставлятиме в рандомні місця в матриці одинички(створюватиме графи) основуючись на заданій щільності
 
-    if test == None or test == False:  # при тестуванні (test == True) ми не виводимо інформацію про кількість ребер задля візуальної чистоти терміналу
-        print(f"The max possible number of  directed acyclic graph is {len(adjacency_matrix)}")
+    # print(f"The max possible number of  directed acyclic graph is {len(adjacency_matrix)}")
 
     for key in adjacency_matrix:
         chance = random.uniform(0, 1)
@@ -41,25 +38,22 @@ def graph_generator(vertex: int, density: float, test=None):
         if adjacency_matrix[key] == 1:
             graphs.append(key)
 
-    if test == None or test == False:  # при тестуванні (test == True) ми не виводимо інформацію про кількість ребер задля візуальної чистоти терміналу
-        print(f"The number of generated graph is {len(graphs)}")
+    # print(f"The number of generated graph is {len(graphs)}")
 
     # пишемо "перекладач" з матриці суміжності у списки суміжності
 
     adjacency_lst = {}  # список суміжності
     for key in adjacency_matrix:
-        if not key in adjacency_lst and adjacency_matrix[key] == 1:  # для кожної вершини,  якщо її немає в нашому списку суміжності, то ми створюємо пустий список, в який згодом додаватимемо вершини, до яких від першої йдуть ребра
+        if not key in adjacency_lst:
             adjacency_lst.setdefault(key[0], []).append(key[1])
-        elif not key in adjacency_lst and not adjacency_matrix[key]:
-            adjacency_lst.setdefault(key[0], [])
         else:
-            adjacency_lst[key[0]].append(key[1])  # до списку суміжності для кожної вершини ми додаємо вершину, до якої є ребро
+            adjacency_lst[key[0]].append(key[1])
 
-    adjacency_lst.setdefault(vertex, [])
     return graphs, adjacency_lst
 
 
-def get_vertex_matrix(graph: list[tuple[int, int]]) -> dict[int, bool]:
+
+def get_vertex(graph: list[tuple[int, int]]) -> dict[int, bool]:
     """
     це функція яка з отриманого графа виписує всі вершини які там є і чи відвідали ми цю вершину
 
@@ -71,17 +65,6 @@ def get_vertex_matrix(graph: list[tuple[int, int]]) -> dict[int, bool]:
         vertexes.append(first)
         vertexes.append(second)
 
-    vertexes_dict = {vertex: False for vertex in vertexes}
-
-    return vertexes_dict
-
-
-
-def get_vertex_list(adjacency_lst: dict[int, int]) -> dict[int, bool]:
-    vertexes = []
-    for vertex in adjacency_lst.keys():
-        vertexes.append(vertex)
-
-    vertexes_dict = {vertex: False for vertex in vertexes}
+    vertexes_dict = {k: False for k in vertexes}
 
     return vertexes_dict
